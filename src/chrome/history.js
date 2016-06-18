6376ce85ff41713694d22a6e6a9734cc9733e0b3
FallenMax <fallenmax@gmail.com>
2016-06-19 00:50:07 +0800

    0.2.5

--- src/chrome/history.js
+++ src/chrome/history.js
@@ -5,6 +5,12 @@ window.updateHistory = (function() {
 1. Minimal UI (removed shadow/radius)
 2. Adds support for sites like *.github.io
 (for nerds: they scrolls an article container instead of whole window)
+
+0.2.5:
+1. Better article extraction algorithm
+2. Works on sites using iframe
+3. Fix: cannot scroll to bottom on some sites
+3. Smaller font size
 `
 
   return history.split(/\n\n+/).filter(Boolean)
