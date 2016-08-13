0f8cdfd3fb67704e8484e7cebe2e2c934444cb7d
FallenMax <fallenmax@gmail.com>
2016-08-13 22:44:44 +0800

    get rid of history

--- src/chrome/background.js
+++ src/chrome/background.js
@@ -14,21 +14,3 @@ const action = function(command) {
 }
 chrome.browserAction.onClicked.addListener(tab => action('toggle'))
 chrome.commands.onCommand.addListener(command => action(command))
-
-
-// notification on update
-chrome.runtime.onInstalled.addListener(function(details) {
-  if (details.reason == 'update') {
-    const curVer = chrome.runtime.getManifest().version
-    const updateMsg = window.updateHistory[curVer]
-    const prevVer = details.previousVersion
-    if (curVer !== prevVer && updateMsg) {
-      chrome.notifications.create({
-        type: 'basic',
-        iconUrl: './icon.png',
-        title: `Smart TOC upgraded to ${curVer}`,
-        message: updateMsg
-      })
-    }
-  }
-})
