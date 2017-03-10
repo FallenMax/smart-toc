cf2ffbb88a31152cb15d15532a7564fa4efe58e1
王丛伟 <wangcw@huoli.com>
2017-03-10 09:58:56 +0800

    filter invisible heading
    thanks e-cloud

--- src/js/helpers/extract.js
+++ src/js/helpers/extract.js
@@ -12,7 +12,7 @@ const pathToTop = function(elem, maxLvl = -1) {
 
 const isStrongAlsoHeading = function(rootElement = document) {
   return false
-    // return rootElement.querySelectorAll('p > strong:only-child').length > 3
+  // return rootElement.querySelectorAll('p > strong:only-child').length > 3
 }
 
 export const extractArticle = function(rootElement = document) {
@@ -31,20 +31,20 @@ export const extractArticle = function(rootElement = document) {
 
   // weigh nodes by factor: "selector", "distance from this node"
   const weights = {
-    h1: [0, 100, 60, 40, 30, 25, 22].map(s => (s * 0.4)),
+    h1: [0, 100, 60, 40, 30, 25, 22].map(s => s * 0.4),
     h2: [0, 100, 60, 40, 30, 25, 22],
-    h3: [0, 100, 60, 40, 30, 25, 22].map(s => (s * 0.5)),
-    h4: [0, 100, 60, 40, 30, 25, 22].map(s => (s * 0.5 * 0.5)),
-    h5: [0, 100, 60, 40, 30, 25, 22].map(s => (s * 0.5 * 0.5 * 0.5)),
-    h6: [0, 100, 60, 40, 30, 25, 22].map(s => (s * 0.5 * 0.5 * 0.5 * 0.5)),
+    h3: [0, 100, 60, 40, 30, 25, 22].map(s => s * 0.5),
+    h4: [0, 100, 60, 40, 30, 25, 22].map(s => s * 0.5 * 0.5),
+    h5: [0, 100, 60, 40, 30, 25, 22].map(s => s * 0.5 * 0.5 * 0.5),
+    h6: [0, 100, 60, 40, 30, 25, 22].map(s => s * 0.5 * 0.5 * 0.5 * 0.5),
     article: [500],
     '.article': [500],
     '.content': [101],
-    'sidebar': [-500],
+    sidebar: [-500],
     '.sidebar': [-500],
-    'aside': [-500],
+    aside: [-500],
     '.aside': [-500],
-    'nav': [-500],
+    nav: [-500],
     '.nav': [-500],
     '.navigation': [-500],
     '.toc': [-500],
@@ -57,15 +57,15 @@ export const extractArticle = function(rootElement = document) {
       elems: [].slice.apply(rootElement.querySelectorAll(selector))
     }))
     .forEach(({ selector, elems }) =>
-      elems.forEach(elem =>
-        updateScore(elem, weights[selector])
-      )
-    )
-  const sorted = [...scores].sort((a, b) => (b[1] - a[1]))
+      elems.forEach(elem => updateScore(elem, weights[selector])))
+  const sorted = [...scores].sort((a, b) => b[1] - a[1])
 
   // reweigh top 5 nodes by factor:  "take-lots-vertical-space", "contain-less-links", "too-narrow"
-  let candicates = sorted.slice(0, 5).filter(Boolean).map(([elem, score]) => ({ elem, score }))
-  let isTooNarrow = e => (e.scrollWidth < 400) // rule out sidebars
+  let candicates = sorted
+    .slice(0, 5)
+    .filter(Boolean)
+    .map(([elem, score]) => ({ elem, score }))
+  let isTooNarrow = e => e.scrollWidth < 400 // rule out sidebars
   candicates.forEach(c => {
     if (isTooNarrow(c.elem)) {
       c.isNarrow = true
@@ -81,11 +81,12 @@ export const extractArticle = function(rootElement = document) {
   const reweighted = candicates
     .map(({ elem, score }) => [
       elem,
-      score * Math.log(elem.scrollHeight / (elem.querySelectorAll('a').length || 1)),
+      score *
+        Math.log(elem.scrollHeight / (elem.querySelectorAll('a').length || 1)),
       elem.scrollHeight,
       elem.querySelectorAll('a').length
     ])
-    .sort((a, b) => (b[1] - a[1]))
+    .sort((a, b) => b[1] - a[1])
   const article = reweighted.length ? reweighted[0][0] : null
   if (__DEV__) {
     log(sorted)
@@ -101,25 +102,34 @@ export const extractHeadings = function(article) {
 
   // what to be considered as headings
   const tags = ['H1', 'H2', 'H3', 'H4', 'H5', 'H6'].concat(
-    isStrongAlsoHeading(article) ? 'STRONG' : [])
-  const tagWeight = tag => ({ H1: 4, H2: 9, H3: 9, H4: 10, H5: 10, H6: 10, STRONG: 10 }[tag])
-  const isMostlyVisible = headings => (headings.filter(h => h.offsetHeight).length >= headings.length * 0.5)
-  const headingGroup = tags.map(tag => [].slice.apply(article.getElementsByTagName(tag)))
+    isStrongAlsoHeading(article) ? 'STRONG' : []
+  )
+  const tagWeight = tag =>
+    ({ H1: 4, H2: 9, H3: 9, H4: 10, H5: 10, H6: 10, STRONG: 10 })[tag]
+  const isVisible = elem => elem.offsetHeight !== 0
+  const isGroupVisible = headings =>
+    headings.filter(isVisible).length >= headings.length * 0.5
+  const headingGroup = tags
+    .map(tag => [].slice.apply(article.getElementsByTagName(tag)))
     .map((headings, i) => ({
       elems: headings,
       tag: tags[i],
       score: headings.length * tagWeight(tags[i])
     }))
     .filter(heading => heading.score >= 10)
-    .filter(heading => isMostlyVisible(heading.elems))
+    .filter(heading => isGroupVisible(heading.elems))
     .slice(0, 3)
 
   // use document sequence
   const validTags = headingGroup.map(headings => headings.tag)
-  const acceptNode = node => validTags.includes(node.tagName) ? NodeFilter.FILTER_ACCEPT : NodeFilter.FILTER_SKIP
+  const acceptNode = node =>
+    validTags.includes(node.tagName) && isVisible(node)
+      ? NodeFilter.FILTER_ACCEPT
+      : NodeFilter.FILTER_SKIP
   const treeWalker = document.createTreeWalker(
     article,
-    NodeFilter.SHOW_ELEMENT, { acceptNode }
+    NodeFilter.SHOW_ELEMENT,
+    { acceptNode }
   )
   const headings = []
   while (treeWalker.nextNode()) {
@@ -130,7 +140,7 @@ export const extractHeadings = function(article) {
     })
   }
   if (__DEV__) {
-    const toElems = g => (g ? g.elems : [])
+    const toElems = g => g ? g.elems : []
     draw(toElems(headingGroup[0]), 'blue')
     draw(toElems(headingGroup[1]), 'green')
     draw(toElems(headingGroup[2]), 'yellow')
