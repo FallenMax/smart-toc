49884fd8bfb84fe8de619b8618070a58be8a9924
FallenMax <FallenMax@gmail.com>
2017-08-21 17:29:11 +0800

    update build config for commonjs deps

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,7 @@
 import replace from 'rollup-plugin-replace'
 import string from 'rollup-plugin-string'
+import nodeResolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
 
 export default {
   entry: 'src/js/index.js',
@@ -7,6 +9,8 @@ export default {
   moduleName: 'smarttoc',
   dest: 'dist/toc.js',
   plugins: [
+    nodeResolve({ main: true, browser: true }),
+    commonjs(),
     string({ include: '**/*.css' }),
     replace({ __DEV__: !!process.env.DEV })
   ]
